e0f20f177d14967cc80e0630b694b84438877869
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-02-06 00:16:58 -0600

    Add another SetItem function

--- Scripts/Inventory.cs
+++ Scripts/Inventory.cs
@@ -2,12 +2,17 @@
 
 public class Inventory
 {
-	private InventorySlot[,] InventorySlots { get; set; }
+	private InventorySlot[] InventorySlots { get; set; }
 	private GridContainer GridContainer { get; set; }
 	private int Padding { get; set; } = 10;
+	private int Columns { get; set; }
+	private int Rows { get; set; }
 
 	public Inventory(Node parent, int columns = 9, int rows = 5)
 	{
+		Columns = columns;
+		Rows = rows;
+
 		var panelContainer = new PanelContainer();
 
 		var marginContainer = new MarginContainer();
@@ -21,17 +26,17 @@ public class Inventory
 
 		marginContainer.AddChild(GridContainer);
 
-		InventorySlots = new InventorySlot[columns, rows];
+		InventorySlots = new InventorySlot[rows * columns];
 
-		for (int x = 0; x < rows; x++)
-			for (int y = 0; y < columns; y++)
-				InventorySlots[y, x] = new InventorySlot(GridContainer);
+		for (int i = 0; i < InventorySlots.Length; i++)
+			InventorySlots[i] = new InventorySlot(GridContainer);
 
 		parent.AddChild(panelContainer);
 	}
 
-	public void SetItem(int x, int y, string name)
-	{
-		InventorySlots[x, y].SetItem(name);
-	}
+	public void SetItem(int i, string name) =>
+		InventorySlots[i].SetItem(name);
+
+	public void SetItem(int x, int y, string name) =>
+		SetItem(x + y * Columns, name);
 }
