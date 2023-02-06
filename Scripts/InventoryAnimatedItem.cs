36a82d4b236eeadb788be6e926252d85017a88c8
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-02-06 01:36:40 -0600

    remove code thats not needed anymore

--- Scripts/InventoryAnimatedItem.cs
+++ Scripts/InventoryAnimatedItem.cs
@@ -17,12 +17,6 @@ public class InventoryAnimatedItem : InventoryItem
 
 		parent.AddChild(AnimatedSprite2D);
 
-		// Ensure sprite is at index 0 so labels can be drawn over it if needed
-		// Another way of doing this could be to set the Z-index of the AnimatedSprite2D node
-		// or set the layer for the Label node. Although this has not been tested and may
-		// not work.
-		parent.MoveChild(AnimatedSprite2D, 0);
-
 		// Label for keeping track of the item stack counts.
 		//var label = new Label { Text = "0" };
 		//parent.AddChild(label);
