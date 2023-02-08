7e25e7f3cddf5c50489917c439bffe6cea77f813
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-02-08 02:01:58 -0600

    remove unused code

--- Scripts/InventoryAnimatedItem.cs
+++ Scripts/InventoryAnimatedItem.cs
@@ -17,10 +17,6 @@ public class InventoryAnimatedItem : InventoryItem
 		AnimatedSprite2D.Play();
 
 		parent.AddChild(AnimatedSprite2D);
-
-		// Label for keeping track of the item stack counts.
-		//var label = new Label { Text = "0" };
-		//parent.AddChild(label);
 	}
 
 	public override void QueueFreeGraphic() => AnimatedSprite2D.QueueFree();
