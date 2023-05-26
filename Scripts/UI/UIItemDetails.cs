0473979a3c3e5b5a5655f33844c1010d9c5f8158
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-05-26 17:47:21 -0500

    Update UIItemDetails.cs

--- Scripts/UI/UIItemDetails.cs
+++ Scripts/UI/UIItemDetails.cs
@@ -4,94 +4,86 @@ namespace Inventory;
 
 public class UIItemDetails
 {
-    public Control Parent { get; set; }
-    private Label LabelName { get; set; }
-    private Label LabelCategory { get; set; }
-    private Label LabelDescription { get; set; }
-    public GridContainer GridContainer { get; set; }
-    protected PanelContainer PanelContainer { get; set; }
-    protected LayoutPreset LayoutPreset { get; set; }
+    Label labelName;
+    Label labelCategory;
+    Label labelDescription;
 
-    private Control ControlPivot { get; set; }
-    private StyleBox PanelStyleBoxVisible { get; set; }
+    PanelContainer panelContainer;
+    Control controlPivot;
 
     public UIItemDetails(Node parent)
     {
-        LayoutPreset = LayoutPreset.TopLeft;
-
         CreateUI(parent);
-
-        // Create labels
-        LabelName = DefaultLabel();
-
-        LabelCategory = DefaultLabel();
-        LabelCategory.AddThemeFontSizeOverride("font_size", 16);
-
-        LabelDescription = DefaultLabel();
-        LabelDescription.AddThemeFontSizeOverride("font_size", 12);
-
-        GridContainer.AddChild(LabelName);
-        GridContainer.AddChild(LabelCategory);
-        GridContainer.AddChild(LabelDescription);
     }
 
-    public void Clear()
+	public void Clear()
     {
-        LabelName.Text = string.Empty;
-        LabelCategory.Text = string.Empty;
-        LabelDescription.Text = string.Empty;
+        labelName.Text = string.Empty;
+        labelCategory.Text = string.Empty;
+        labelDescription.Text = string.Empty;
     }
 
     public void ChangeItem(Item item)
     {
         if (item != null)
         {
-            LabelName.Text = item.Type.Name;
-            LabelCategory.Text = item.Type.ItemCategory.ToString();
-            LabelDescription.Text = item.Type.Description;
+            labelName.Text = item.Type.Name;
+            labelCategory.Text = item.Type.ItemCategory.ToString();
+            labelDescription.Text = item.Type.Description;
         }
     }
 
     public void SetAnchor(LayoutPreset preset)
     {
-        LayoutPreset = preset;
-        PanelContainer.SetAnchorsAndOffsetsPreset(preset);
-        ControlPivot.SetAnchorsAndOffsetsPreset(preset);
+        panelContainer.SetAnchorsAndOffsetsPreset(preset);
+        controlPivot.SetAnchorsAndOffsetsPreset(preset);
     }
-    private void CreateUI(Node parent)
+
+    void CreateUI(Node parent)
     {
         // Setup inventory UI
-        ControlPivot = new Control();
-        PanelContainer = new PanelContainer();
-        GridContainer = new GridContainer();
-
-        PanelStyleBoxVisible = PanelContainer.GetThemeStylebox("panel");
+        controlPivot = new Control();
+        panelContainer = new PanelContainer();
+        var gridContainer = new GridContainer();
 
         var marginContainer = new MarginContainer();
         marginContainer.AddMargin(10);
 
-        GridContainer.Columns = 1;
+        gridContainer.Columns = 1;
         
-        PanelContainer.AddChild(marginContainer);
-        marginContainer.AddChild(GridContainer);
-        ControlPivot.AddChild(PanelContainer);
-        parent.AddChild(ControlPivot);
-    }
+        panelContainer.AddChild(marginContainer);
+        marginContainer.AddChild(gridContainer);
+        controlPivot.AddChild(panelContainer);
+        parent.AddChild(controlPivot);
 
-    public static Label DefaultLabel()
-    {
-        Label label = new Label
-        {
-            HorizontalAlignment = HorizontalAlignment.Left,
-            VerticalAlignment = VerticalAlignment.Bottom,
-            SizeFlagsVertical = Control.SizeFlags.Fill,
-            MouseFilter = Control.MouseFilterEnum.Ignore // ignored by default but just in case Godot changes it in the future
+		// Create labels
+		labelName = DefaultLabel();
+
+		labelCategory = DefaultLabel();
+		labelCategory.AddThemeFontSizeOverride("font_size", 16);
+
+		labelDescription = DefaultLabel();
+		labelDescription.AddThemeFontSizeOverride("font_size", 12);
+
+		gridContainer.AddChild(labelName);
+		gridContainer.AddChild(labelCategory);
+		gridContainer.AddChild(labelDescription);
+	}
+
+	static Label DefaultLabel()
+	{
+		var label = new Label
+		{
+			HorizontalAlignment = HorizontalAlignment.Left,
+			VerticalAlignment = VerticalAlignment.Bottom,
+			SizeFlagsVertical = SizeFlags.Fill,
+			MouseFilter = MouseFilterEnum.Ignore // ignored by default but just in case Godot changes it in the future
         };
 
-        label.AddThemeColorOverride("font_shadow_color", Colors.Black);
-        label.AddThemeConstantOverride("shadow_outline_size", 3);
-        label.AddThemeFontSizeOverride("font_size", 20);
+		label.AddThemeColorOverride("font_shadow_color", Colors.Black);
+		label.AddThemeConstantOverride("shadow_outline_size", 3);
+		label.AddThemeFontSizeOverride("font_size", 20);
 
-        return label;
-    }
+		return label;
+	}
 }
